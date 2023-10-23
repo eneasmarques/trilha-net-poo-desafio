037f6c96d612159c374c9509015b6d2fd3b34083
eneasmarques <eneasmarques@outlook.com.br>
2023-10-23 16:22:04 -0300

    Solução Desafio Trilha .NET POO

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,25 +1,25 @@
 namespace DesafioPOO.Models
 {
+    using System.Net.Http.Headers;
+
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        protected Smartphone(string numero, string modelo, string imei, int memoria)
         {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            this.Numero = numero;
+            this.Modelo = modelo;
+            this.IMEI = imei;
+            this.Memoria = memoria;
         }
 
-        public void Ligar()
-        {
-            Console.WriteLine("Ligando...");
-        }
+        public static void Ligar() => Console.WriteLine("Ligando...");
 
-        public void ReceberLigacao()
-        {
-            Console.WriteLine("Recebendo ligação...");
-        }
+        public static void ReceberLigacao() => Console.WriteLine("Recebendo ligação...");
 
         public abstract void InstalarAplicativo(string nomeApp);
     }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "1111111", memoria: 64);
+Smartphone.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphonne:");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "Modelo 1", imei: "1111111", memoria: 64);
+Smartphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
